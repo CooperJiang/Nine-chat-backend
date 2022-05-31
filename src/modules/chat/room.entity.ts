b74d64be3a1d0c0f96a6c6613289bbd830d73067
jianglongyan <longyan.jiang_tic@baozun.com>
2022-05-31 22:39:04 +0800

    Docs: update readme

--- src/modules/chat/room.entity.ts
+++ src/modules/chat/room.entity.ts
@@ -12,7 +12,7 @@ export class RoomEntity extends BaseEntity {
   @Column({ length: 255, nullable: true, comment: '房间logo' })
   room_logo: string;
 
-  @Column({ length: 255, comment: '房间名称' })
+  @Column({ length: 20, comment: '房间名称' })
   room_name: string;
 
   @Column({ default: 1, comment: '房间是否需要密码 1:公开 2:加密' })
@@ -21,7 +21,7 @@ export class RoomEntity extends BaseEntity {
   @Column({ length: 255, nullable: true, comment: '房间密码' })
   room_password: string;
 
-  @Column({ length: 255, default: '房间空空如也呢', comment: '房间公告' })
+  @Column({ length: 512, default: '房间空空如也呢', comment: '房间公告' })
   room_notice: string;
 
   @Column({ length: 255, nullable: true, comment: '房间背景图片' })
